e2b18282de952f214793924c2e9ea84153f08282
Lindon Roberts <lindon.roberts@maths.ox.ac.uk>
2019-10-04 11:43:07 +1000

    Added basic scripts

---
+++ build_static_site.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+/usr/local/bin/hugo
+
+# cd into public (linked to github.io page) and push changes
+
---
+++ run_test_server.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+/usr/local/bin/hugo server
+
+# Navigate to http://localhost:1313/ to view automatically-updated page
+
